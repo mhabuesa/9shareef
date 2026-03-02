e12e38554c7692fa8237e067a1c3f50fd73edff5
mhabuesa <mhabuesa@gmail.com>
2026-03-02 22:16:16 +0600

    Quiz Result

--- app/Http/Controllers/Quiz/QuizController.php
+++ app/Http/Controllers/Quiz/QuizController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Quiz;
 use App\Http\Controllers\Controller;
 use App\Models\QuizAnswer;
 use App\Models\QuizInfo;
+use App\Models\Socialize;
 use App\Models\SocialPic;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -39,7 +40,7 @@ class QuizController extends Controller
 
     public function result()
     {
-        $cover = SocialPic::first()->cover_pic;
+        $cover = Socialize::first()->cover_pic;
         $winners = QuizAnswer::where('qualified', '1')->get();
         return view('quiz.result', compact('cover', 'winners'));
     }
